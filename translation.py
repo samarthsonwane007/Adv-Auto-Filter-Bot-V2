de86cb3821f852b68e7b63886576243989274dad
samarthsonwane007 <76512588+samarthsonwane007@users.noreply.github.com>
2021-09-11 12:45:36 +0530

    Add files via upload

---
+++ translation.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) @AlbertEinsteinTG
+
+class Translation(object):
+    
+    START_TEXT = """<b>Hey {}!!</b>
+<i>𝗔𝗺 𝗝𝘂𝘀𝘁 𝗔 𝗔𝗱𝘃𝗮𝗻𝗰𝗲 𝗔𝘂𝘁𝗼 𝗙𝗶𝗻𝗱𝗲𝗿 𝗕𝗼𝘁....😉
+
+ 𝗮𝗻𝗱 𝐈 𝗺 𝗰𝘂𝗿𝗿𝗲𝗻𝘁𝗹𝘆 𝗶𝗻 𝗯𝗲𝘁𝗮 𝗺𝗼𝗱𝗲🔥🔥😝
+ 
+   🔰🅒🅗🅐🅝🅝🅔🅛🔰: @taless01
+
+𝗕𝗼𝘁 𝗱𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿 :@umeshask
+
+</i>"""    
+    HELP_TEXT = """
+<b><i><u>How To Use Me!?</u></i></b>
+
+<i>
+-> Well, you can't use me. You don't have permission to use me.
+-> Because i only work for My Master {@umeshask}
+</i>
+
+<b>Bot Commands (Works Only In Groups) :</b>
+
+    -> <code>/add chat_id</code>
+                OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
+     <code>/add @Username</code>
+     
+    -> <code>/del chat_id</code>
+                OR                  - To disconnect A Group With A Channel
+     <code>/del @Username</code>
+     
+    -> <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
+    
+    -> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
+
+            -> <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
+            
+            -> <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
+
+            -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
+            
+            -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
+            
+  Made by @umeshask 😎
+"""
+    
+    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Finder Bot</code>
+    
+<b>➥ Creator</b> : <b><i><a href="https://t.me/umeshask">Mera Naam</a></i></b>
+
+<b>➥ Language</b> : <code>Python3</code>
+
+<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+
+"""
