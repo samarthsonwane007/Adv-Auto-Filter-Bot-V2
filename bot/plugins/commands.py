9d467428910733b6f9a08606b2d4bae446758049
samarthsonwane007 <76512588+samarthsonwane007@users.noreply.github.com>
2021-09-11 13:04:43 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Developers', url="https://t.me/joinchat/r_CIbg-zn5JjZmZl"
                                 )
                         ]
                     ]
@@ -47,10 +47,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
+        InlineKeyboardButton('Developers', url='https://t.me/joinchat/r_CIbg-zn5JjZmZl'),
+        InlineKeyboardButton('Source Code 🧾', url ='https://t.me/joinchat/r_CIbg-zn5JjZmZl')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Support 🛠', url='https://t.me/joinchat/r_CIbg-zn5JjZmZl')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
